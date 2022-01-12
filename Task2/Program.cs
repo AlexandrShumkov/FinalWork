71f1eb949ebf74fc70cfa71418098435a28b13a9
AlexandrShumkov <alshumkov2@yandex.ru>
2022-01-12 15:26:53 +0300

    правка

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел
+using System.Linq;
 
 int[] arr = new int[10];
 Random rnd = new Random();
@@ -7,10 +8,10 @@ for (int i = 0; i < arr.Length; i++)
     arr[i] = rnd.Next(1, 100);
     Console.Write(arr[i] + " ");
 }
-    Console.ReadKey();
-    Console.WriteLine();
+Console.ReadKey();
+Console.WriteLine();
 for (int i = 0; i < arr.Length; i++)
 {
     if (arr[i] % 2 == 0) Console.Write(arr[i] + " ");
-    else Console.Write(" ");
+    else Console.Write("");
 }
